47de83ca1d09cd8ff3ee0e801ad99b352ab9e59f
akazwz <akazwz@pm.me>
2022-09-18 11:46:05 +0800

    add more api

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,7 +21,40 @@ export const SignupApi = async(data: { username: string, password: string }) => 
 }
 
 export const GetPathApi = async(token: string, parentID: string) => {
-	return fetch(`${API_HOST}/file/${parentID}/path`, {
+	return fetch(`${API_HOST}/folders/${parentID}/path`, {
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		},
+	})
+}
+
+export const GetFoldersAndFilesByParentIDApi = async(token: string, parentID: string) => {
+	return fetch(`${API_HOST}/folders/${parentID}/all`, {
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		},
+	})
+}
+
+export const GetFoldersByParentIDApi = async(token: string, parentID: string) => {
+	return fetch(`${API_HOST}/folders/${parentID}/folders`, {
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		},
+	})
+}
+
+export const GetFilesByParentIDApi = async(token: string, parentID: string) => {
+	return fetch(`${API_HOST}/folders/${parentID}/files`, {
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		},
+	})
+}
+
+export const CreateFolderApi = async(token: string, parentID: string, folderName: string) => {
+	return fetch(`${API_HOST}/folders/${parentID}/${folderName}`, {
+		method: 'POST',
 		headers: {
 			'Authorization': `Bearer ${token}`,
 		},
